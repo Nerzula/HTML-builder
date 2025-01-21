c76e713156f86d77a61a0988a32a13fb6e07b7a7
Alex <alexzloid@gmail.com>
2025-01-21 15:04:15 +0300

    refactor: remove Sync metod

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,50 +1,64 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
-function copyDir() {
-   const src = path.join(__dirname, 'files');
-   const build = path.join(__dirname, 'files-copy');
+async function copyDir() {
+	const src = path.join(__dirname, 'files');
+	const build = path.join(__dirname, 'files-copy');
 
-   if (fs.existsSync(build)) {
-      removeFolderSync(build);
-   }
+	try {
+		if (await exists(build)) {
+			await removeFolder(build);
+		}
 
-   fs.mkdirSync(build, { recursive: true });
+		await fs.mkdir(build, { recursive: true });
 
-   copyContent(src, build);
+		await copyContent(src, build);
 
-   console.log('Copying completed!');
+		console.log('Copying completed!');
+	} catch (error) {
+		console.error(`Error during copying: ${error.message}`);
+	}
 }
 
-function copyContent(src, dest) {
-   const items = fs.readdirSync(src, { withFileTypes: true });
+async function copyContent(src, dest) {
+	const items = await fs.readdir(src, { withFileTypes: true });
 
-   for (const item of items) {
-      const srcPath = path.join(src, item.name);
-      const destPath = path.join(dest, item.name);
+	for (const item of items) {
+		const srcPath = path.join(src, item.name);
+		const destPath = path.join(dest, item.name);
 
-      if (item.isFile()) {
-         fs.copyFileSync(srcPath, destPath);
-      } else if (item.isDirectory()) {
-         fs.mkdirSync(destPath, { recursive: true });
-         copyContent(srcPath, destPath);
-      }
-   }
+		if (item.isFile()) {
+			await fs.copyFile(srcPath, destPath);
+		} else if (item.isDirectory()) {
+			await fs.mkdir(destPath, { recursive: true });
+			await copyContent(srcPath, destPath);
+		}
+	}
 }
 
-function removeFolderSync(folderPath) {
-   const items = fs.readdirSync(folderPath, { withFileTypes: true });
+async function removeFolder(folderPath) {
+	const items = await fs.readdir(folderPath, { withFileTypes: true });
 
-   for (const item of items) {
-      const itemPath = path.join(folderPath, item.name);
+	for (const item of items) {
+		const itemPath = path.join(folderPath, item.name);
 
-      if (item.isDirectory()) {
-         removeFolderSync(itemPath);
-      } else {
-         fs.unlinkSync(itemPath);
-      }
-   }
-   fs.rmdirSync(folderPath);
+		if (item.isDirectory()) {
+			await removeFolder(itemPath);
+		} else {
+			await fs.unlink(itemPath);
+		}
+	}
+
+	await fs.rmdir(folderPath);
+}
+
+async function exists(path) {
+	try {
+		await fs.access(path);
+		return true;
+	} catch {
+		return false;
+	}
 }
 
 copyDir();
